577bedefe91a35cb13b46da46a95be267fc174bc
skitak <bas2205@live.fr>
2018-07-05 00:08:16 +0200

    filter layout finished

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,3 +1,5 @@
+import { EHOSTUNREACH } from "constants";
+
 export default function filter(){
     $(".filter").on("click",function(e){
         if ($(this).hasClass("active"))
@@ -13,6 +15,7 @@ function openMenuFilter(){
     $(".filter").addClass("active")
     $(".filters-menu").addClass("active")
     $(".projects").addClass("filtered")
+    removeContainers()
 }
 
 function closeFilterMenu(){
@@ -20,6 +23,35 @@ function closeFilterMenu(){
     $(".filters-menu").removeClass("active")
     $(".projects").removeClass("filtered")
     $(".filter-button").removeClass("selected")
+    addContainers()
+}
+
+function removeContainers(){
+    $(".project").each(function(obj){
+        $(this).appendTo(".projects")
+    })
+    $(".primary-project-wrapper").remove()
+    $(".projects hr").remove()
+    $(".secondary-projects-wrapper").remove()
+    $(".all-projects-wrapper").remove()
+    
+}
+
+function addContainers(){
+    $(".projects").append(
+    "<div class='primary-project-wrapper'>" +
+    "</div>" +
+    "<hr/>" +
+    "<div class='secondary-projects-wrapper'>" +
+    "</div>" +
+    "<div class='all-projects-wrapper'>" +
+    "</div>" )
+    $(".projects > .project").first().appendTo(".primary-project-wrapper")
+    $(".projects > .project").first().appendTo(".secondary-projects-wrapper")
+    $(".projects > .project").first().appendTo(".secondary-projects-wrapper")
+    $(".projects > .project").each(function(){
+        $(this).appendTo(".all-projects-wrapper")
+    })
 }
 
 
